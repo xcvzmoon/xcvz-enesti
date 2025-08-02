a11af8a937aa4db6ec113bd991a5a9e42db0d5d8
Mon Albert Gamil <mrgamilmonalbert@gmail.com>
2025-08-02 09:53:30 +0800

    fix: update pool config schema

--- server/types/pool-config.ts
+++ server/types/pool-config.ts
@@ -1,9 +1,9 @@
 import * as z from 'zod/v4';
 
 export const poolConfigSchema = z.object({
-  host: z.optional(z.string()),
-  port: z.number().transform((val) => Number(val)),
-  database: z.optional(z.string()),
-  user: z.optional(z.string()),
-  password: z.optional(z.string()),
+  host: z.string(),
+  port: z.string().transform((val) => Number(val)),
+  database: z.string(),
+  user: z.string(),
+  password: z.string(),
 });
